838d21df9d2ebdf3f32ddd3b66ae2ed6210c6716
Kyle Williams <williams.kyleg@gmail.com>
2021-01-30 14:45:43 -0500

    post like and unlike

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -159,4 +159,41 @@ router.put('/unlike/:id', auth, async (req, res) => {
   }
 });
 
+// @route   DELETE api/posts/comment/:id/:comment_id
+// @desc    Delete comment from post
+// @access  Private
+router.delete('/comment/:id/:comment_id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    // Pull out comment from post
+    const comment = post.comments.find(
+      comment => comment.id === req.params.comment_id
+    );
+
+    // Make sure comment exists
+    if (!comment) {
+      return res.status(404).json({ msg: 'Comment not found' });
+    }
+
+    // Check user
+    if (comment.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: 'User not authorized' });
+    }
+
+    const indexToRemove = post.comments
+      .map(comment => comment.user.toString())
+      .indexOf(req.user.id);
+
+    post.comments.splice(indexToRemove, 1);
+
+    await post.save();
+
+    res.json(post.comments);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send('Server Error');
+  }
+});
+
 module.exports = router;
