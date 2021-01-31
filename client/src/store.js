6268f30aeef6005e58b5e3a756019226566776a3
Kyle Williams <williams.kyleg@gmail.com>
2021-01-31 17:04:03 -0500

    created redux store

---
+++ client/src/store.js
@@ -0,0 +1,16 @@
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
+import rootReducer from './reducers'; // Since file is index.js it does not need to be declared in the file path.
+
+const initialState = {};
+
+const middleware = [thunk];
+
+const store = createStore(
+  rootReducer,
+  initialState,
+  composeWithDevTools(applyMiddleware(...middleware))
+);
+
+export default store;
