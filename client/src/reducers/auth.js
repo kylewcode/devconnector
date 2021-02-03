6286309058098544f24e59110d1bc3f8c9bc2a9c
Kyle Williams <williams.kyleg@gmail.com>
2021-02-03 13:46:44 -0500

    load user and set auth token

--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -1,6 +1,8 @@
 import {
   REGISTER_SUCCESS,
-  REGISTER_FAIL
+  REGISTER_FAIL,
+  USER_LOADED,
+  AUTH_ERROR,
 } from '../actions/types';
 
 const initialState = {
@@ -11,10 +13,17 @@ const initialState = {
 };
 
 // eslint-disable-next-line import/no-anonymous-default-export
-export default function(state = initialState, action ) {
+export default function (state = initialState, action) {
   const { type, payload } = action;
 
   switch (type) {
+    case USER_LOADED:
+      return {
+        ...state,
+        isAuthenticated: true,
+        loading: false,
+        user: payload,
+      }
     case REGISTER_SUCCESS:
       localStorage.setItem('token', payload.token);
       return {
@@ -22,14 +31,15 @@ export default function(state = initialState, action ) {
         ...payload,
         isAuthenticated: true,
         loading: false,
-      }
+      };
     case REGISTER_FAIL:
+    case AUTH_ERROR:
       localStorage.removeItem('token');
       return {
         token: null,
         isAuthenticated: false,
         loading: false,
-      }
+      };
     default:
       return state;
   }
