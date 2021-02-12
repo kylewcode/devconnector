8c6e214e4aa8d16f2cf23690ba9a81ae83c38f26
Kyle Williams <williams.kyleg@gmail.com>
2021-02-12 14:48:05 -0500

    github repos in profile

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -5,6 +5,7 @@ import ProfileTop from './ProfileTop';
 import ProfileAbout from './ProfileAbout';
 import ProfileExperience from './ProfileExperience';
 import ProfileEducation from './ProfileEducation';
+import ProfileGithub from './ProfileGithub';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { getProfileById } from '../../actions/profile';
@@ -36,6 +37,7 @@ const Profile = ({
           <div className='profile-grid my-1'>
             <ProfileTop profile={profile} />
             <ProfileAbout profile={profile} />
+
             <div className='profile-exp bg-white p-2'>
               <h2 className='text-primary'>Experience</h2>
               {profile.experience.length > 0 ? (
@@ -48,6 +50,7 @@ const Profile = ({
                 <h4>No experience credentials</h4>
               )}
             </div>
+
             <div className='profile-edu bg-white p-2'>
               <h2 className='text-primary'>Education</h2>
               {profile.education.length > 0 ? (
@@ -60,6 +63,10 @@ const Profile = ({
                 <h4>No education credentials</h4>
               )}
             </div>
+
+            {profile.githubusername ? (
+              <ProfileGithub username={profile.githubusername}/>
+            ) : null}
           </div>
         </Fragment>
       )}
