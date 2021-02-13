e2d0e37f75dbd279072ab94aa3d1da7968693160
Kyle Williams <williams.kyleg@gmail.com>
2021-02-13 10:38:42 -0500

    Post Item Component

--- client/src/components/post/PostItem.js
+++ client/src/components/post/PostItem.js
@@ -7,44 +7,38 @@ import { connect } from 'react-redux';
 const PostItem = ({
   auth,
   post: { _id, text, name, avatar, user, likes, comments, date },
-}) => {
-  return (
-    <div className='post bg-white p-1 my-1'>
-      <div>
-        <a href='profile.html'>
-          <img
-            className='round-img'
-            src='https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50?s=200'
-            alt=''
-          />
-          <h4>John Doe</h4>
-        </a>
-      </div>
-      <div>
-        <p className='my-1'>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint possimus
-          corporis sunt necessitatibus! Minus nesciunt soluta suscipit nobis.
-          Amet accusamus distinctio cupiditate blanditiis dolor? Illo
-          perferendis eveniet cum cupiditate aliquam?
-        </p>
-        <p className='post-date'>Posted on 04/16/2019</p>
-        <button type='button' className='btn btn-light'>
-          <i className='fas fa-thumbs-up'></i>
-          <span>4</span>
-        </button>
-        <button type='button' className='btn btn-light'>
-          <i className='fas fa-thumbs-down'></i>
-        </button>
-        <a href='post.html' className='btn btn-primary'>
-          Discussion <span className='comment-count'>2</span>
-        </a>
+}) => (
+  <div className='post bg-white p-1 my-1'>
+    <div>
+      <Link to={`profile/${user}`}>
+        <img className='round-img' src={avatar} alt='' />
+        <h4>{name}</h4>
+      </Link>
+    </div>
+    <div>
+      <p className='my-1'>{text}</p>
+      <p className='post-date'>Posted on {formatDate(date)}</p>
+      <button type='button' className='btn btn-light'>
+        <i className='fas fa-thumbs-up'></i>
+        <span>{likes.length > 0 ? likes.length : null}</span>
+      </button>
+      <button type='button' className='btn btn-light'>
+        <i className='fas fa-thumbs-down'></i>
+      </button>
+      <Link to={`post/${_id}`} className='btn btn-primary'>
+        Discussion{' '}
+        <span className='comment-count'>
+          {comments.length > 0 ? comments.length : '0'}
+        </span>
+      </Link>
+      {auth.isAuthenticated && auth.user._id === user ? (
         <button type='button' className='btn btn-danger'>
           <i className='fas fa-times'></i>
         </button>
-      </div>
+      ) : null}
     </div>
-  );
-};
+  </div>
+);
 
 PostItem.propTypes = {
   post: PropTypes.object.isRequired,
